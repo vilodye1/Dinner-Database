927ae3a592e9d9e381862f967e92a05f3a9cc794
vilodye1 <vilodye@gmail.com>
2023-06-07 14:13:52 -0400

    install axios and API request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,30 @@
         <input type="text" placeholder="Search..." class="rounded border-2 border-gray-200 p-2 w-full" />
 
         <div class="flex gap-2 mt-2">
-            <router-link :to="{name: 'byLetter', params: {letter}}" :key="letter" v-for="letter of letters">
+            <router-link 
+            :to="{name: 'byLetter', params: {letter}}" 
+            :key="letter" 
+            v-for="letter of letters">
                 <p class="hover:text-orange-300">{{ letter }}</p>
             </router-link>
         </div>
+
     </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import { useMealStore } from '../stores/MealStore';
+import axiosClient from '../axiosClient.js'
 
-const meals = useMealStore()
+
 
 // search bar letters
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+// Registers a callback to be called after the component has been mounted.
+onMounted(async () => {
+    const response = await axiosClient.get('/list.php?i=list')
+
+})
 </script>
